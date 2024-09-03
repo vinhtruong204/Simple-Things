ae66a672c717449e49c5a334990037f5614efb4e
Trương Quang Vinh <vt3839796@gmail.com>
2024-09-03 09:41:19 +0700

    Refactor code move enemy

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,11 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField] private float enemySpeed = 2.0f;
+    private float enemySpeed = 5.0f;
     private Rigidbody2D enemyRb;
+
+    // Prevent player change direction twice and get stuck in the wall
     private bool isChangingDirection = false;
 
     // Start is called before the first frame update
@@ -14,40 +18,60 @@ public class EnemyMovement : MonoBehaviour
         enemyRb = GetComponentInParent<Rigidbody2D>();
     }
 
+    private bool IsFacingRight()
+    {
+        return transform.parent.localScale.x > Mathf.Epsilon; // 0.0001f
+    }
+
     // Update is called once per frame
     private void FixedUpdate()
     {
+        ResetBoolChangingDirection();
+
+        // Move enemy horizontal
+        ChangeMoveDirection();
+    }
+
+    private void ResetBoolChangingDirection()
+    {
         isChangingDirection = false;
-        MoveHorizontal();
     }
 
-    private void MoveHorizontal()
+    private void ChangeMoveDirection()
     {
+        // Get current rigidbody2d velocity
         Vector2 velocity = enemyRb.velocity;
-        if (IsFacingRight())
-        {
-            velocity.x = enemySpeed;
-        }
-        else
-        {
-            velocity.x = -enemySpeed;
-        }
 
+        // Change move direction depend on enemy's direction
+        velocity.x = IsFacingRight() ? enemySpeed : -enemySpeed;
+
+        // Set new velocity
         enemyRb.velocity = velocity;
-    }
-
-    private bool IsFacingRight()
-    {
-        return transform.parent.localScale.x > Mathf.Epsilon;
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
+        // If enemy is changing direction 
         if (isChangingDirection) return;
-        isChangingDirection = true;
-        Vector3 scale = transform.parent.localScale;
-        scale.x *= -1.0f;
-        transform.parent.localScale = scale;
 
+        // Prevent this method from being called and change direction on the next call
+        isChangingDirection = true;
+
+
+        // Flip enemy horizontal
+        Flip();
+    }
+
+
+    private void Flip()
+    {
+        // Get parent game object scale
+        Vector3 scale = transform.parent.localScale;
+
+        // Flip horizontal
+        scale.x *= -1.0f;
+
+        // Set new parent game object scale
+        transform.parent.localScale = scale;
     }
 }
