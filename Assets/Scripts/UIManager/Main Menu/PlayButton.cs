0771a6423c9cebf4c62027090fa503f2aa4b173c
Trương Quang Vinh <vt3839796@gmail.com>
2024-10-11 21:34:57 +0700

    Test slider loading scene, upgrade editor version

--- Assets/Scripts/UIManager/Main Menu/PlayButton.cs
+++ Assets/Scripts/UIManager/Main Menu/PlayButton.cs
@@ -2,11 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayButton : BaseButton
 {
+    public Slider slider;
+    public TMP_Text percentText;
+    private new void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+        base.Awake();
+    }
     protected override void OnClick()
     {
-        SceneManager.LoadScene(1);
+        StartCoroutine(LoadSceneAsynchronously(1));
+    }
+
+    IEnumerator LoadSceneAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            slider.value = progress;
+            percentText.text = progress * 100.0f + "%";
+
+            yield return null;
+        }
     }
 }
