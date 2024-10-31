183ae03fb55b394d6c75e6034bbbe71135f1f6de
Trương Quang Vinh <vt3839796@gmail.com>
2024-10-31 21:55:11 +0700

    Added scriptable objects for enemy vision

--- Assets/Scripts/Enemies/Base/EnemyMovement.cs
+++ Assets/Scripts/Enemies/Base/EnemyMovement.cs
@@ -78,6 +78,4 @@ public class EnemyMovement : MonoBehaviour
         enemyAnimation.Flip();
     }
 
-
-
 }
--- Assets/Scripts/Spawners/Object Pooling/Bomb/BombPool.cs
+++ Assets/Scripts/Spawners/Object Pooling/Bomb/BombPool.cs
@@ -8,13 +8,12 @@ using System;
 public class BombPool : ObjectPooling
 {
     private readonly int amountInitial = 5;
-    private AsyncOperationHandle<GameObject> handle;
 
     // Start is called before the first frame update
     private void Start()
     {
-        handle = Addressables.LoadAssetAsync<GameObject>(BombString.BOMB_PREFABS_PATH);
-        handle.Completed += Load_Completed;
+        Addressables.LoadAssetAsync<GameObject>(BombString.BOMB_PREFABS_PATH)
+        .Completed += Load_Completed;
     }
 
     private void Load_Completed(AsyncOperationHandle<GameObject> operationHandle)
@@ -29,10 +28,5 @@ public class BombPool : ObjectPooling
 
         transformParent = transform; // Set location to instantiate bomb object
         InitialPool(amountInitial); // Start instantiate object with start size of pool is 10
-    }
-
-    private void OnDestroy()
-    {
-        handle.Release();
     }
 }
